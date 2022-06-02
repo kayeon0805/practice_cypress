b26b123f50c1df96f20904d18befff90b28760a5
kayeon <kayeon8855@gmail.com>
2022-06-02 19:19:39 +0900

    - 버튼 클릭 시 count 1 감소 테스트

--- js-counter-test/cypress/e2e/counter.cy.js
+++ js-counter-test/cypress/e2e/counter.cy.js
@@ -1,6 +1,6 @@
 // - [o] counter의 초기값은 0이다.
 // - [o] + 버튼을 클릭 시 count가 1증가한다.
-// - [] - 버튼을 클릭 시 count가 1감소한다.
+// - [o] - 버튼을 클릭 시 count가 1감소한다.
 // - [] + 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)
 // - [] - 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)
 // - [] reset 버튼을 클릭 시 counter가 0으로 초기화된다.
@@ -26,6 +26,23 @@ describe("example counter app", () => {
                 cy.get("#value")
                     .invoke("text")
                     .should("eq", String(preValue + 1));
+            });
+    });
+
+    it("- 버튼을 클릭 시 count가 1감소한다.", () => {
+        // + 버튼을 클릭해서 값을 1로 만든다.
+        // 기존 값인 1을 가져오고,
+        // - 버튼을 클릭한 다음에
+        // 변화된 값이 - 1인지 체크
+        cy.get(".increase-btn").click();
+        cy.get("#value")
+            .invoke("text")
+            .then((value) => {
+                const preValue = Number(value);
+                cy.get(".decrease-btn").click();
+                cy.get("#value")
+                    .invoke("text")
+                    .should("eq", String(preValue - 1));
             });
     });
 });
