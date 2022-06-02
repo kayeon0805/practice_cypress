f785bf33709285040de30a0c75edb4bfd8139893
kayeon <kayeon8855@gmail.com>
2022-06-02 21:50:11 +0900

    AC버튼 테스트

--- js-calculator-test/cypress/e2e/calcurator.cy.js
+++ js-calculator-test/cypress/e2e/calcurator.cy.js
@@ -2,7 +2,7 @@
 // - [o]] 2개의 숫자에 대해 뺄셈이 가능하다.
 // - [o] 2개의 숫자에 대해 곱셈이 가능하다.
 // - [o] 2개의 숫자에 대해 나눗셈이 가능하다.
-// - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
+// - [0] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
 // - [ ] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
 // - [ ] 계산 결과를 표현할 때 소수점 이하는 버린다.
 
@@ -41,5 +41,11 @@ describe("계산기 애플리케이션 테스트", () => {
         cy.get(".digit").contains("1").click();
         cy.get(".operation").contains("=").click();
         cy.get("#total").should("have.text", "2");
+    });
+
+    it("AC(All Clear)버튼을 누르면 0으로 초기화 한다.", () => {
+        cy.get(".digit").contains("2").click();
+        cy.get(".modifier").contains("AC").click();
+        cy.get("#total").should("have.text", "0");
     });
 });
