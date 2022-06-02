690ef44603a48c69f643fdcdbeb2b1cc37d4bbf0
kayeon <kayeon8855@gmail.com>
2022-06-02 21:45:59 +0900

    뺄셈 테스트

--- js-calculator-test/cypress/e2e/calcurator.cy.js
+++ js-calculator-test/cypress/e2e/calcurator.cy.js
@@ -1,5 +1,5 @@
 // - [o] 2개의 숫자에 대해 덧셈이 가능하다.
-// - [ ] 2개의 숫자에 대해 뺄셈이 가능하다.
+// - [o]] 2개의 숫자에 대해 뺄셈이 가능하다.
 // - [ ] 2개의 숫자에 대해 곱셈이 가능하다.
 // - [ ] 2개의 숫자에 대해 나눗셈이 가능하다.
 // - [ ] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
@@ -17,5 +17,13 @@ describe("계산기 애플리케이션 테스트", () => {
         cy.get(".digit").contains("1").click();
         cy.get(".operation").contains("=").click();
         cy.get("#total").should("have.text", "3");
+    });
+
+    it("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
+        cy.get(".digit").contains("2").click();
+        cy.get(".operation").contains("-").click();
+        cy.get(".digit").contains("1").click();
+        cy.get(".operation").contains("=").click();
+        cy.get("#total").should("have.text", "1");
     });
 });
