31313a2955642fb6cdb67036fabbf5ba646bf710
kayeon <kayeon8855@gmail.com>
2022-06-02 19:25:56 +0900

    count 최소값 0 테스트

--- js-counter-test/cypress/e2e/counter.cy.js
+++ js-counter-test/cypress/e2e/counter.cy.js
@@ -2,7 +2,7 @@
 // - [o] + 버튼을 클릭 시 count가 1증가한다.
 // - [o] - 버튼을 클릭 시 count가 1감소한다.
 // - [o] + 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)
-// - [] - 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)
+// - [o] - 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)
 // - [] reset 버튼을 클릭 시 counter가 0으로 초기화된다.
 
 describe("example counter app", () => {
@@ -51,5 +51,10 @@ describe("example counter app", () => {
             cy.get(".increase-btn").click();
         }
         cy.get("#value").invoke("text").should("eq", "10");
+    });
+
+    it("- 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)", () => {
+        cy.get(".decrease-btn").click();
+        cy.get("#value").invoke("text").should("eq", "0");
     });
 });
