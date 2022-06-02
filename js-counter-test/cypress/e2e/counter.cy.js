8fd33a9c0c6b246603858037bdf0c84c378d4d2f
kayeon <kayeon8855@gmail.com>
2022-06-02 19:22:14 +0900

    count 최대값 10 테스트

--- js-counter-test/cypress/e2e/counter.cy.js
+++ js-counter-test/cypress/e2e/counter.cy.js
@@ -1,7 +1,7 @@
 // - [o] counter의 초기값은 0이다.
 // - [o] + 버튼을 클릭 시 count가 1증가한다.
 // - [o] - 버튼을 클릭 시 count가 1감소한다.
-// - [] + 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)
+// - [o] + 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)
 // - [] - 버튼을 클릭 시 count가 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0)
 // - [] reset 버튼을 클릭 시 counter가 0으로 초기화된다.
 
@@ -44,5 +44,12 @@ describe("example counter app", () => {
                     .invoke("text")
                     .should("eq", String(preValue - 1));
             });
+    });
+
+    it("+ 버튼을 클릭 시 count가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)", () => {
+        for (let i = 0; i < 11; i++) {
+            cy.get(".increase-btn").click();
+        }
+        cy.get("#value").invoke("text").should("eq", "10");
     });
 });
